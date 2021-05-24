7408ad556519bce49fb3ecc4d9f5b984066448ba
윤희수 <hsyoon@iparking.co.kr>
2021-05-24 11:55:24 +0900

    생성

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MultiStep />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MultiStep from './components/MultiStep.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MultiStep
   }
 }
 </script>
---
+++ src/components/MultiStep.vue
@@ -0,0 +1,79 @@
+<template>
+    <div v-if="inputData">
+        {{ inputData.title }}
+        <dl v-if="currentData">
+            <dt>({{ currentData.itemId }}) {{ currentData.title }}</dt>
+            <dd v-if="currentData.formType == 1">
+                <span v-for="item in currentData.options" :key="item.id">
+                    <input :id="'type_checkbox'+item.id" type="checkbox" name="type_checkbox">  
+                    <label :for="'type_checkbox'+item.id">{{ item.text }}</label>
+                </span>
+            </dd>
+            <dd v-if="currentData.formType == 2">
+                <span v-for="item in currentData.options" :key="item.id">
+                    <input :id="'type_radio'+item.id" type="radio" name="type_radio">  
+                    <label :for="'type_radio'+item.id">{{ item.text }}</label>
+                </span>
+            </dd>
+            <dd v-if="currentData.formType == 3">
+                <input type="text">  
+            </dd>
+            <dd v-if="currentData.formType == 4">
+                <select name="" id="">
+                    <option v-for="item in currentData.options" :key="item.id" :value="item.id">{{ item.text }}</option>    
+                </select> 
+            </dd>
+        </dl>
+        <button :disabled="currentDataIdx == 0" @click="prevPage()">&lt;</button>
+        <button :disabled="currentDataIdx == 3" @click="nextPage()">&gt;</button>
+    </div>
+</template>
+
+<script>
+import { InputData } from '../assets/input.ts';
+
+export default {
+    name: 'MultiStep',
+    components: {
+    },
+    data() {
+        return {
+            inputData: {},
+            currentData: {},
+            currentDataIdx: 0,
+            formType: [
+                { id: 1, type: 'Checkbox' },
+                { id: 2, type: 'Radio' },
+                { id: 3, type: 'Text Input' },
+                { id: 4, type: 'Selectbox' },
+            ],
+        }
+    },
+    beforeCreate() {},
+    created() {
+        this.inputData = InputData,
+        this.currentData = InputData.items[0],
+        console.log('currentData', this.currentData)
+    },
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    beforeUnmount() {},
+    unmounted() {},
+    computed: {
+    },
+    methods: {
+        prevPage(){
+            this.currentDataIdx = this.currentDataIdx-1;
+            this.currentData = InputData.items[this.currentDataIdx];
+        },
+        nextPage(){
+            this.currentDataIdx = this.currentDataIdx+1;
+            this.currentData = InputData.items[this.currentDataIdx];
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
